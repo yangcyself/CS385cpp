032a9c63d94b0a7987a96d2005061d5d144358a8
Yangcyself <13520183356@139.com>
2019-06-16 20:28:42 +0800

    changed the way of generating image

--- generageDataset.sh
+++ generageDataset.sh
@@ -1,9 +1,14 @@
-# bash
-# for I in {1..8}
-# do 
-#     ./processImage ../FDDB-folds/FDDB-fold-0$I-ellipseList.txt ./out/train/ 0${I}_
-# done
+bash
+for I in {1..4}
+do 
+    ./processImage ../FDDB-folds/FDDB-fold-0$I-ellipseList.txt ./out/train/ 0${I}_ 1
+done
 
-./processImage ../FDDB-folds/FDDB-fold-09-ellipseList.txt ./out/test/ 09_
-./processImage ../FDDB-folds/FDDB-fold-10-ellipseList.txt ./out/test/ 10_
+for I in {5..8}
+do 
+    ./processImage ../FDDB-folds/FDDB-fold-0$I-ellipseList.txt ./out/train/ 0${I}_ 0
+done
+
+./processImage ../FDDB-folds/FDDB-fold-09-ellipseList.txt ./out/test/ 09_ 0
+./processImage ../FDDB-folds/FDDB-fold-10-ellipseList.txt ./out/test/ 10_ 1
 
