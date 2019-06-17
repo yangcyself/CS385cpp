b4f9e02b70b279a2533be71773277629647f0cec
Yangcyself <13520183356@139.com>
2019-06-17 16:54:52 +0800

    added the trainmod flag related functions

--- cnn/basicOps.h
+++ cnn/basicOps.h
@@ -37,11 +37,14 @@ public:
     void feedData(const Tensor& d){data = d;}
     Tensor forward(){return data;}
     void backward(const Tensor& in){}
+    void train(){}
+    void test(){}
     // void update(double step){}
 };
 
 /**
  * Variable class, contains the variable, and update according to the input gradient tensor.
+ * 
  */
 class Variable:public Operator{
 private:
@@ -51,12 +54,16 @@ public:
     ~Variable(){}
     Tensor forward(){return data;}
     void initData(const Tensor d){data = d;}  // here should not pass by ref, because the data is going to change
-    void backward(const Tensor& in){data = data.eleadd(in);}
+
+    void backward(const Tensor& in){assert(trainmod); data = data.eleadd(in);}
+    void train(){trainmode = true;}
+    void test(){trainmode = false;}
 };
 
 
-
-
+/**
+ * simply add two tensors together, because the operation is simple, it do not worty the space for cache.
+ */
 class Add:public Operator{
 private:
     std::shared_ptr<Operator> a;
@@ -66,6 +73,27 @@ public:
     ~Add(){}
     Tensor forward(){return a->forward().eleadd(b->forward());}
     void backward(const Tensor& in){a->backward(in); b->backward(in);}
+
+    void train(){trainmode = true;a->train();b->train();}
+    void test(){trainmode = false;a->test();b->test();}
+};
+
+/**
+ * The conv operator class, the b is the ready to use kernel, outC * H*W*inC
+ */
+class Conv:public Operator{
+private:
+    std::shared_ptr<Operator>a;
+    std::shared_ptr<Operator>b;
+    Tensor ca;
+    Tensor cb;
+public:
+    Conv(Operator& aa,Operator& bb):a(&aa,&null_deleter),b(&bb,&null_deleter){}
+    ~Conv(){}
+    Tensor forward(){return a->forward().conv( b->forward());}
+    void backward(const Tensor& in);
+
+
 };
 
 } /* namespace convnn */
--- cnn/operator.h
+++ cnn/operator.h
@@ -11,12 +11,15 @@ namespace convnn{
 /**
  * The Operator class, the parent class of all the network structures
  * Contains the pointer of upstream operators and thus forms a computation graph
+ * Do not have the notion of optimizer. thus, each operator is optimizer itself
+ * Each operator can only cache one last output, used for compute gradient. 
  * Every time in forward propagation, call the forward propagation of all upstream operators
  *      and return the result
  * Every time in backward propagation, calculate the gradient an recursively pass to upstream
  * (deparcated) Every time call update, calculate the gradient an recursively and update a step
  * 
- * TODO: add a dirty flag and cache operator's output to optimize
+ * 
+ *  have a dirty flag and cache operator's output to optimize
 */
 
 class Operator{
@@ -24,13 +27,20 @@ public:
     using vector = Eigen::VectorXd; 
     using matrix = Eigen::Matrix<double,  Eigen::Dynamic, Eigen::Dynamic,Eigen::RowMajor>;
 
-private:
-    
+protected:
+    bool trainmod;
 public:
-    Operator(){}
+    Operator():trainmod(true){}
     virtual ~Operator(){}
     virtual Tensor forward() =0;
     virtual void backward(const Tensor& in) =0; 
+
+    /**
+     * The two functions to set and unset the trainmod flags in the whole network
+     */
+    virtual void train();
+    virtual void test();
+
     // virtual void update(double step) =0;
 };
 } /* namespace logistic */
