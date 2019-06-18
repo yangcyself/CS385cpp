840d126ec61cc1cec649c0c9b0686602e53495d9
yangcyself <13520183356@139.com>
2019-06-18 10:36:40 +0800

    added mkdir in generate dataset

--- generageDataset.sh
+++ generageDataset.sh
@@ -1,8 +1,12 @@
 # bash
-# for I in {1..8}
-# do 
-#     ./processImage ../FDDB-folds/FDDB-fold-0$I-ellipseList.txt ./out/train/ 0${I}_
-# done
+mkdir -p out/test/neg
+mkdir -p out/test/pos
+mkdir -p out/train/neg
+mkdir -p out/train/pos
+for I in {1..8}
+do 
+    ./processImage ../FDDB-folds/FDDB-fold-0$I-ellipseList.txt ./out/train/ 0${I}_
+done
 
 ./processImage ../FDDB-folds/FDDB-fold-09-ellipseList.txt ./out/test/ 09_
 ./processImage ../FDDB-folds/FDDB-fold-10-ellipseList.txt ./out/test/ 10_
